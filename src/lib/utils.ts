8d6037fd6d9bb7b65bc05180e75505c9602853cc
Pranjal Jain <pranjalworkon@gmail.com>
2025-09-05 17:05:26 +0530

    feat: added metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,34 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from "next"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const appURL = process.env.NEXT_PUBLIC_APP_URL!
+
+export function constructMetadata(): Metadata {
+  return {
+    metadataBase: new URL(appURL),
+    title: {
+      default: "Json Bro",
+      template: "Json Bro | %s",
+    },
+    description:
+      "JSON Bro helps you to utilise the fake API response from the various endpoints that helps streamline your development process. Use the JSON Bro API to generate realistic data for testing and prototyping in a proper structure.",
+    applicationName: "Json Bro",
+    keywords: [
+      "json formatter",
+      "Fake api",
+      "json viewer",
+      "json editor",
+      "json validator",
+      "json beautifier",
+      "API testing",
+      "json parser",
+      "json schema",
+    ],
+    authors: [{ name: "Pranjal Jain", url: "https://github.com/Jain-Pranjal/JSON-Bro" }],
+  }
 }
