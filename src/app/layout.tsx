b1cbc3c6242d1837f07b0ce30d81b512fcc29d3a
Pranjal Jain <pranjalworkon@gmail.com>
2025-10-27 10:59:52 +0530

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { constructMetadata } from '@/lib/utils' 
-
+import type { Metadata } from 'next'
+import './globals.css'
+import { constructMetadata } from '@/lib/utils'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = constructMetadata()
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-       <body>
-        {/* <ClientProvider> */}
-        {children}
-        {/* </ClientProvider> */}
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {/* <ClientProvider> */}
+                {children}
+                <Analytics />
+                {/* </ClientProvider> */}
+            </body>
+        </html>
+    )
 }
